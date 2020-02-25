d53f03db64673536f4bad9937413b83a7f6f1c87
Francesco Chicchiriccò <ilgrosso@apache.org>
2020-02-25 09:22:23 +0100

    Clean up

--- connector/src/main/java/net/tirasa/connid/bundles/okta/utils/OktaEventType.java
+++ connector/src/main/java/net/tirasa/connid/bundles/okta/utils/OktaEventType.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2019 ConnId (connid-dev@googlegroups.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.tirasa.connid.bundles.okta.utils;
 
 import java.util.Arrays;
@@ -5,24 +20,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import org.identityconnectors.framework.common.objects.SyncDeltaType;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 public enum OktaEventType {
 
     APPLICATION_LIFECYCLE_ACTIVATE("application.lifecycle.activate", SyncDeltaType.CREATE_OR_UPDATE),
