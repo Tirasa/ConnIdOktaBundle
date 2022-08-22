4bdde2d92acebca5bae4819348af4211e3e56c36
Hiroyuki Wada <h2-wada@nri.co.jp>
2022-08-22 15:35:14 +0900

    OKTA-11 Improve API error handling (#8)

--- connector/src/main/java/net/tirasa/connid/bundles/okta/utils/OktaUtils.java
+++ connector/src/main/java/net/tirasa/connid/bundles/okta/utils/OktaUtils.java
@@ -18,8 +18,15 @@ package net.tirasa.connid.bundles.okta.utils;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
+import com.okta.sdk.resource.ResourceException;
 import org.identityconnectors.common.logging.Log;
+import org.identityconnectors.framework.common.exceptions.AlreadyExistsException;
 import org.identityconnectors.framework.common.exceptions.ConnectorException;
+import org.identityconnectors.framework.common.exceptions.ConnectorSecurityException;
+import org.identityconnectors.framework.common.exceptions.InvalidAttributeValueException;
+import org.identityconnectors.framework.common.exceptions.PermissionDeniedException;
+import org.identityconnectors.framework.common.exceptions.RetryableException;
+import org.identityconnectors.framework.common.exceptions.UnknownUidException;
 
 public class OktaUtils {
 
@@ -37,9 +44,38 @@ public class OktaUtils {
         throw new ConnectorException(message, ex);
     }
 
-    public static void wrapGeneralError(final String message, final Exception ex) {
+    public static void wrapGeneralError(final String message, Exception ex) {
         LOG.error(ex, message);
+        if (ex instanceof ResourceException) {
+            ex = wrapResourceException((ResourceException) ex);
+        }
         throw ConnectorException.wrap(ex);
+    }
+
+    private static Exception wrapResourceException(final ResourceException e) {
+        // Error handling based on status code
+        // https://developer.okta.com/docs/reference/error-codes/
+        switch (e.getStatus()) {
+            case 400:
+                if (e.getError().getCode().equals("E0000001")) {
+                    boolean isAlreadyExists = e.getError().getCauses().stream()
+                            .anyMatch(x -> x.getSummary().endsWith("An object with this field already exists in the current organization"));
+                    if (isAlreadyExists) {
+                        return new AlreadyExistsException(e);
+                    }
+                } else {
+                    return new InvalidAttributeValueException(e);
+                }
+            case 401:
+                return new ConnectorSecurityException(e);
+            case 403:
+                return new PermissionDeniedException(e);
+            case 404:
+                return new UnknownUidException(e);
+            case 429:
+                return RetryableException.wrap(e.getMessage(), e);
+        }
+        return e;
     }
 
     public static String buildSearchQuery(final String attributeName, final String attributeValue) {
